87f497def937aa9d8cc2f3c5f65a4787ad653f97
Cezar Crintea <cezar.crintea@gmail.com>
2025-08-03 21:01:20 +0300

    Task 3

--- main.py
+++ main.py
@@ -1,102 +1,116 @@
-"""Task 2"""
+"""Task 3"""
 
 import json
 import time
-from typing import Any, Dict, List
+from typing import Any, Dict, Tuple
 
 from qdrant_client import QdrantClient, models
+
+
+QUERIES_FILE = "dataset/queries_embeddings.json"
 
 QDRANT_HOST = "localhost"
 QDRANT_PORT = 6333
 COLLECTION_NAME = "arxiv_papers"
 
-QUERIES_FILE = "dataset/queries_embeddings.json"
-
-K = 10
-HNSW_EF_VALUES = [10, 20, 50, 100, 200]
+k = 10
 
 
-def evaluate_hnsw_ef(
-    client: QdrantClient, k: int, hnsw_ef_values: List[int], dataset: Dict[str, Any]
-):
-    """
-    Evaluate the HNSW algorithm with different ef values on a given dataset.
-    """
-    ground_truth = {}
+def evaluate(client: QdrantClient, dataset: Dict[str, Any]) -> Dict[str, float]:
+    ann_times = []
+    knn_times = []
+    ann_precision = []
+
     for text, embedding in dataset.items():
-        print(f"Ground truth / Processing query: {text}")
+        print(f"Processing query: {text}")
+        start_time_ann = time.time()
+        ann_result = client.query_points(
+            collection_name=COLLECTION_NAME, query=embedding, limit=k
+        ).points
+        ann_time = time.time() - start_time_ann
+        ann_times.append(ann_time)
+        print(f"ANN query time: {ann_time * 1000:.2f} ms")
 
+        start_time_knn = time.time()
         knn_result = client.query_points(
             collection_name=COLLECTION_NAME,
             query=embedding,
             limit=k,
             search_params=models.SearchParams(exact=True),
         ).points
+        knn_time = time.time() - start_time_knn
+        knn_times.append(knn_time)
+        print(f"Exact k-NN query time: {knn_time * 1000:.2f} ms")
 
-        point_ids = [point.id for point in knn_result]
-        ground_truth[text] = point_ids
+        ann_ids = set(item.id for item in ann_result)
+        knn_ids = set(item.id for item in knn_result)
+        precision = len(ann_ids.intersection(knn_ids)) / k
+        ann_precision.append(precision)
+        print(f"Precision@{k}: {precision:.4f}\n")
 
-    results = []
-
-    for hnsw_ef_value in hnsw_ef_values:
-        ann_times = []
-        ann_precisions = []
-
-        for text, embedding in dataset.items():
-            print(f"HNSW with ef={hnsw_ef_value} / Processing query: {text}")
-
-            start_time_ann = time.time()
-            ann_result = client.query_points(
-                collection_name=COLLECTION_NAME,
-                query=embedding,
-                limit=k,
-                search_params=models.SearchParams(hnsw_ef=hnsw_ef_value),
-            ).points
-            ann_time = time.time() - start_time_ann
-            ann_times.append(ann_time)
-
-            ann_ids = {point.id for point in ann_result}
-            ground_truth_ids = ground_truth[text]
-            precision = len(ann_ids.intersection(ground_truth_ids)) / k
-            ann_precisions.append(precision)
-
-        avg_precision = sum(ann_precisions) / len(dataset)
-        avg_ann_time = sum(ann_times) / len(dataset)
-        result = {
-            "hnsw_ef": hnsw_ef_value,
-            "avg_precision": avg_precision,
-            "avg_query_time_ms": avg_ann_time * 1000,
-        }
-        results.append(result)
-
-    return results
+    avg_precision = sum(ann_precision) / len(dataset)
+    avg_ann_time = sum(ann_times) / len(dataset)
+    avg_knn_time = sum(knn_times) / len(dataset)
+    result = {
+        "avg_precision": avg_precision,
+        "avg_ann_time": avg_ann_time,
+        "avg_knn_time": avg_knn_time,
+    }
+    return result
 
 
-def result_formatting(results: List[Dict[str, Any]]):
-    """Format and print the evaluation results.
-
-    Args:
-        results (List[Dict[str, Any]]): List of evaluation results.
-    """
+def display_results(results: Dict[Tuple[int, int], Dict[str, float]]):
+    """Display the results of the evaluation."""
     print("\nResults:")
-    for result in results:
-        hnsw_ef = result["hnsw_ef"]
-        avg_precision = result["avg_precision"]
-        avg_ann_time = result["avg_query_time_ms"]
-        print(f"HNSW ef={hnsw_ef} {avg_precision:.4f} {avg_ann_time:.2f} ms")
+    print("-" * 80)
+    print(
+        f"{'m':<5} {'ef_construct':<15} {'Precision@' + str(k):<15} {'ANN Time (ms)':<15} {'k-NN Time (ms)':<15}"
+    )
+    print("-" * 80)
+
+    for (m, ef_construct), metrics in results.items():
+        print(
+            f"{m:<5} {ef_construct:<15} "
+            f"{metrics['avg_precision']:.4f}{'':<9} "
+            f"{metrics['avg_ann_time'] * 1000:.2f}{'':<10} "
+            f"{metrics['avg_knn_time'] * 1000:.2f}"
+        )
+
+    print("-" * 80)
 
 
 def main():
     """Main function to run the evaluation."""
-
     client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
 
     with open(QUERIES_FILE, "r", encoding="utf-8") as file:
         test_dataset = json.load(file)
 
-    evaluation_results = evaluate_hnsw_ef(client, K, HNSW_EF_VALUES, test_dataset)
+    test_cases = [
+        (8, 50),
+        (8, 100),
+        (16, 32),
+        (16, 50),
+    ]
 
-    result_formatting(evaluation_results)
+    results = {}
+    for m, ef_construct in test_cases:
+        print(f"Running evaluation with m={m}, ef_construct={ef_construct}")
+        print("Updating collection parameters...")
+        start_time = time.time()
+        client.update_collection(
+            collection_name=COLLECTION_NAME,
+            hnsw_config=models.HnswConfigDiff(
+                m=m,
+                ef_construct=ef_construct,
+            ),
+        )
+        end_time = time.time()
+        print(f"Collection parameters updated in {end_time - start_time:.2f} seconds")
+        result = evaluate(client, dataset=test_dataset)
+        results[(m, ef_construct)] = result
+
+    display_results(results)
 
 
 if __name__ == "__main__":
