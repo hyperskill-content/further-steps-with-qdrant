7c04cfcd3690fe442a09fbf9cead14d439017007
Anushka Weerasooriya <Nushbox@gmail.com>
2026-03-24 16:13:31 +0000

    Implement task-3

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import asyncio
 import time
 import json
-from qdrant_client import QdrantClient
-from qdrant_client.models import SearchParams, ScoredPoint
+from qdrant_client import QdrantClient, models
 from typing import List, Dict, Any
 
 QDRANT_HOST = "localhost"
@@ -11,23 +10,50 @@ COLLECTION_NAME = "arxiv_papers"
 QUERIES_FILE = "./dataset/queries_embeddings.json"
 
 client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
+client.update_collection(
+    collection_name=COLLECTION_NAME,
+    optimizer_config=models.OptimizersConfigDiff(),
+    quantization_config=models.ScalarQuantization(
+        scalar=models.ScalarQuantizationConfig(
+            type=models.ScalarType.INT8,
+            quantile=0.99,
+            always_ram=False
+        )
+    )
+)
 
-
-def get_result(k: int, embedding: List[float], exact: bool, hnsw_ef: int | None) -> List[ScoredPoint]:
+def get_result(k: int, embedding: List[float], exact: bool, hnsw_ef: int | None, disable_quantized_search: bool,
+               rescore: bool | None = None, oversampling: float | None = None) -> List[models.ScoredPoint]:
     return client.query_points(
         collection_name=COLLECTION_NAME,
         query=embedding,
-        search_params=SearchParams(exact=exact, hnsw_ef=hnsw_ef),
+        search_params=models.SearchParams(
+            exact=exact,
+            hnsw_ef=hnsw_ef,
+            quantization=models.QuantizationSearchParams(
+                ignore=disable_quantized_search,
+                rescore=rescore,
+                oversampling=oversampling
+            )
+        ),
         limit=k
     ).points
 
 
-def get_ann_result(k: int, embedding: List[float], hnsw_ef: int) -> List[ScoredPoint]:
-    return get_result(k=k, embedding=embedding, exact=False, hnsw_ef=hnsw_ef)
+def get_ann_result(k: int, embedding: List[float], hnsw_ef: int, rescore: bool, oversampling: float) -> List[models.ScoredPoint]:
+    return get_result(
+        k=k,
+        embedding=embedding,
+        exact=False,
+        hnsw_ef=hnsw_ef,
+        disable_quantized_search=False,
+        rescore=rescore,
+        oversampling=oversampling
+    )
 
 
-def get_knn_result(k: int, embedding: List[float]) -> List[ScoredPoint]:
-    return get_result(k=k, embedding=embedding, exact=True, hnsw_ef=None)
+def get_knn_result(k: int, embedding: List[float]) -> List[models.ScoredPoint]:
+    return get_result(k=k, embedding=embedding, exact=True, hnsw_ef=None, disable_quantized_search=True)
 
 
 def display_results(k: int, results: List[Dict[str, Any]]):
@@ -37,6 +63,9 @@ def display_results(k: int, results: List[Dict[str, Any]]):
 
 
 def evaluate_hnsw_ef(k: int, hnsw_ef_values: List[int], test_dataset: Dict[str, Any]):
+    rescore = False
+    oversampling = 2.0
+
     results = []
     precisions = {}
     ann_times = {}
@@ -47,7 +76,7 @@ def evaluate_hnsw_ef(k: int, hnsw_ef_values: List[int], test_dataset: Dict[str, 
 
         for hnsw_ef in hnsw_ef_values:
             start_time_ann = time.time()
-            ann_result = get_ann_result(k, embedding, hnsw_ef)
+            ann_result = get_ann_result(k, embedding, hnsw_ef, rescore, oversampling)
             ann_time = time.time() - start_time_ann
             ann_times.setdefault(hnsw_ef, []).append(ann_time)
 
