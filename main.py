9bfb8f235fa3bd777a0c3db888bd0cac80cbd0a5
Frank <fvreys@outlook.com>
2026-07-31 22:32:39 +0200

    Task1: Result (of 2 runs) and reflection

---
+++ main.py
@@ -0,0 +1,85 @@
+from qdrant_client import QdrantClient, models
+import json
+import time
+from dotenv import load_dotenv
+
+# OK Run the Qdrant Docker image (see stage 1 of the Vector database with Qdrant project);
+# OK Download the test dataset that will be used for the calculation.
+#    It is a JSON file with 100 queries and their corresponding text-embedding-ada-002 embeddings. The queries are stored as the keys, and the embeddings are stored as the values.
+# Load the dataset with the script:
+
+QUERIES_FILE = "C:/Users/frank/PycharmProjects/HyperskillAI/further-steps-with-qdrant/dataset/queries_embeddings.json"
+with open(QUERIES_FILE, 'r', encoding='utf-8') as file:
+    test_dataset = json.load(file)
+
+load_dotenv()
+
+QDRANT_HOST = "localhost"
+QDRANT_PORT = 6333
+COLLECTION_NAME = 'arxiv_papers'
+k = 10
+
+client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
+
+# For each embedding in test_dataset.values() (or each query, embedding pair via test_dataset.items()),
+# Run the exact and the approximate searches, calculate the precision, and log the time for a single query.
+# The precision of a single query can be calculated as
+
+ann_times = []
+knn_times = []
+precisions = []
+for query, embedding in test_dataset.items():
+    # Run exact and approximate searches
+    # Log the average time for a single query
+    # Calculate precision
+    # Append the logged times and the precision to their corresponding lists.
+
+    start_time_ann = time.time()
+    ann_result = client.query_points (
+        collection_name=COLLECTION_NAME,
+        query=embedding,
+        limit=k
+    ).points
+    ann_time = time.time() - start_time_ann
+    ann_times.append(ann_time)
+    # print(f"ANN query time: {ann_time * 1000:.2f} ms")
+
+    start_time_knn = time.time()
+    knn_result = client.query_points(
+        collection_name=COLLECTION_NAME,
+        query=embedding,
+        limit=k,
+        search_params=models.SearchParams(exact=True),
+    ).points
+    knn_time = time.time() - start_time_knn
+    knn_times.append(knn_time)
+    # print(f"KNN query time: {knn_time * 1000:.2f} ms")
+
+    ann_ids = set(item.id for item in ann_result)
+    knn_ids = set(item.id for item in knn_result)
+    precision = len(ann_ids.intersection(knn_ids)) / k
+    precisions.append(precision)
+    # print(f"Precision@{k}: {precision:.4f}")
+
+averages = {
+    "avg_ann_time": sum(ann_times) / len(ann_times),
+    "avg_knn_time": sum(knn_times) / len(knn_times),
+    "avg_precision": sum(precisions) / len(precisions),
+}
+
+# Display the averages and write up a reflection on the obtained results
+def result_formatting(k, avg_precision, avg_ann_time, avg_knn_time):
+    print(f'Average precision@{k}: {avg_precision:.4f}')
+    print(f'Average ANN query time: {avg_ann_time * 1000:.2f} ms')
+    print(f'Average exact k-NN query time: {avg_knn_time * 1000:.2f} ms')
+
+
+if __name__ == "__main__":
+    result_formatting (10, averages["avg_precision"], averages["avg_ann_time"], averages["avg_knn_time"])
+
+    # FILE_PATH = "C:/Users/Frank/Documents/Backup/AI Sp3 data/ml-arxiv-embeddings.json"
+    # path = FILE_PATH or os.getenv("FILE_PATH")
+    #
+    # print(f"Main> Path to use={path}")
+    # load_vectors_to_qdrant(path)
+    #
--- tasks/SOLUTION.md
+++ tasks/SOLUTION.md
@@ -2,8 +2,21 @@
 
 ```
 stdout of the solution script for the task
+Average precision@10: 1.0000
+Average ANN query time: 62.86 ms
+Average exact k-NN query time: 62.89 ms
+
+Average precision@10: 1.0000
+Average ANN query time: 66.96 ms
+Average exact k-NN query time: 65.99 ms
 ```
+
 
 ### Reflection
 
-Which patterns are present? How can they be explained? Describe your findings in detail here.
+1. The precision@10 is 1.0000 for both runs, indicating that the ANN index is able to return the correct results for all queries.
+2. The query times for both ANN and exact k-NN are very close. This means the ANN index is able to return results that are very similar to the exact k-NN results. 
+   * The ANN index has a similar accuracy as the k-NN index for a similar speed, for the given dataset.
+3. Possible we have these good results for the ANN index because the test data set with only 100 embeddings is small.
+   * This means we can use the k-nn index for small datasets, without losing speed 
+   * Or we can use the ANN index for small datasets, without losing accuracy.
